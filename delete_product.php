91738437b5f6771ece59c0c21d1ee22e323eb593
Joony Ngọc <38630115+ngocbao152@users.noreply.github.com>
2018-11-19 22:02:11 +0700

    up

--- delete_product.php
+++
@@ -1,9 +0,0 @@
-<?php 
-include 'config/connect.php';
-include 'function.php';
-$idDelete = $_GET['idDelete'];
-$sql = "DELETE FROM product WHERE id = $idDelete";
-if (mysqli_query($connect, $sql) === TRUE) {
-	redirectPage('list_product.php');
-}
-?>
